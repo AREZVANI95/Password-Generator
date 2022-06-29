28255ef8fb629807c85743b82e0ee3c1838ac1ff
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-06-29 00:53:54 -0400

    Update script.js
    added if statements that check for what the user gets to select between the options of capital - special characters incorporated into their password.

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ var numeric = "numeric"
 var special = "!@#$%^&*()_+-=[]{},./?";
 var passString = "";
 var passResult = "";
+
+var textArea = document.getElementById("password");
+
+function characterTypes() {
+    var passwordCapital = confirm("would you like to include uppercase letters?");
+    var passwordLowercase = confirm("would you like to include lowercase letters?");
+    var passwordNumeric = confirm("would you like to include numbers?");
+    var passwordSpecial = confirm("would you like to include special characters?");
+    if (passwordCapital === true || passwordLowercase === true ||
+        passwordNumeric === true || passwordSpecial === true) {
+
+        if (passwordCapital === true) {
+            passString += capital
+        }
+
+        if (passwordLowercase === true) {
+            passString += lower
+        }
+        if (passwordNumeric === true) {
+            passString += numeric
+        }
+        if (passwordSpecial === true) {
+            passString += special
+        }
+    } else {
+        alert("Please choose at least one character type.")
+        characterTypes()
+    };
+}
