62718658b48f8692c35853e0b7628995c5479993
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-06-30 20:45:34 -0400

    Restarting Fresh for Resubmit
    All the Code in this commit was copied and pasted from the starter code that has been provided to me.

--- script.js
+++ script.js
@@ -1,69 +1,17 @@
-var passwordCapital = "";
-var passwordLowercase = "";
-var passwordNumeric = "";
-var passwordSpecial = "";
-var capital = "ABCDEFGHIJKLMNOPQURSTUVWXYZ"
-var lower = "abcdefghijklmnopqrstuvwxyz"
-var numeric = "0123456789"
-var special = "!@#$%^&*()_+-=[]{},./?";
-var passString = "";
-var passResult = "";
+// Assignment code here
 
-var textArea = document.getElementById("password");
 
-function characterTypes() {
-    var passwordCapital = confirm("would you like to include uppercase letters?");
-    var passwordLowercase = confirm("would you like to include lowercase letters?");
-    var passwordNumeric = confirm("would you like to include numbers?");
-    var passwordSpecial = confirm("would you like to include special characters?");
-    if (passwordCapital === true || passwordLowercase === true ||
-        passwordNumeric === true || passwordSpecial === true) {
-
-        if (passwordCapital === true) {
-            passString += capital
-        }
-
-        if (passwordLowercase === true) {
-            passString += lower
-        }
-        if (passwordNumeric === true) {
-            passString += numeric
-        }
-        if (passwordSpecial === true) {
-            passString += special
-        }
-    } else {
-        alert("Please choose at least one character type.")
-        characterTypes()
-    };
-}
-
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
 function writePassword() {
-  var text = '';
-  function questions() {
-    var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 characters to 128 characters.");
-    if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
-      characterTypes()
-      
-      for (let i = 0; i < passwordLength; i++) {
-        let x = passString[Math.floor(Math.random() * passString.length)]
-        passResult += x;
-      }
-      console.log(passResult);
-      console.log(textArea);
-      text = document.createTextNode(passResult);
-      textArea.innerHTML = '';
-      textArea.appendChild(text);
-      passResult = '';
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    };
+  passwordText.value = password;
 
-  }
-  questions()
 }
 
-generateBtn.addEventListener("click", () => {
-    writePassword();
-  });
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
