90088d3546a60c8feab4f80541a3331a1dbd2dc7
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-06-30 22:39:11 -0400

    Update script.js
    Added if statements that if the chosen variables are selected by the user, it will be added to the password that will be generated.

--- script.js
+++ script.js
@@ -28,10 +28,31 @@ function writePassword() {
 
 // Asks the user to choose the options given to build their random password.
 function generatePassword() {
-  var upper = confirm("Add Uppercase characters to your password?");
-  var lower = confirm("Add Lowercase characters to your password?");
-  var numb = confirm("Add Numbers to your password?");
-  var special = confirm("Add Special characters to your password?");
+  var passUppercase = confirm("Add Uppercase characters to your password?");
+  var passLowercase = confirm("Add Lowercase characters to your password?");
+  var passNumerical = confirm("Add Numbers to your password?");
+  var passSpecialcharacters = confirm("Add Special characters to your password?");
+  console.log("prompts loaded properly");
+  // This will confirm and pass the chosen variables picked by the user to the passString
+  if (upper === true || lower === true || numb === true || special === true) {
+
+    if (passUppercase === true) {
+      passtring += upper
+    }
+
+    if (passLowercase === true) {
+      passtring += lower
+    }
+
+    if (passNumerical === true) {
+      passtring += numb
+    }
+
+    if (passSpecialcharacters === true) {
+      passtring += special
+    }
+
+  }
 
 }
 
