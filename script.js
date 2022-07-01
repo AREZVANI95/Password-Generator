57240cda89f8d7a917b5d0eeb4448f44adf2f4e9
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-06-30 22:24:17 -0400

    Update script.js
    Added the 4 main options the user will be able to choose from when generating their password.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Assignment code here
 
-//Variables for each options the user will choose from. //
+//Variables for each options the user will choose from. 
 var passUppercase = "";
 var passLowercase = "";
 var passNumerical = "";
 var passSpecialcharacters = "";
 
-// Assigning each variable with their corresponding content //
+// Assigning each variable with their corresponding content 
 var upper = "ABCDEFGHIJKLMNOPQURSTUVWXYZ"
 var lower = "abcdefghijklmnopqrstuvwxyz"
-var num = "1234567890"
-var capital = "!@#$%^&*()_+-=[]{},./?"
+var numb = "1234567890"
+var special = "!@#$%^&*()_+-=[]{},./?"
 
-//String and Result variables //
+//String and Result variables 
 var passString = "";
 var passResult = "";
 
@@ -26,7 +26,14 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Asks the user to choose the options given to build their random password.
+function generatePassword() {
+  var upper = confirm("Add Uppercase characters to your password?");
+  var lower = confirm("Add Lowercase characters to your password?");
+  var numb = confirm("Add Numbers to your password?");
+  var special = confirm("Add Special characters to your password?");
 
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
