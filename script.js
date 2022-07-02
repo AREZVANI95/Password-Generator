4439edd36dd16ba551bea35b4c1e2926d8163ad6
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-07-02 00:15:17 -0400

    Fixing Errors and changing palcements
    Fixed some of the errors in the code and changed the position of some lines of code

--- script.js
+++ script.js
@@ -21,60 +21,48 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
 }
+var passwordText = document.querySelector("#password");
 
 // Asks the user to choose the options given to build their random password.
 function generatePassword() {
-  var passLength = prompt("Choose the size of your password. (8-128 characters)");
   var passUppercase = confirm("Add Uppercase characters to your Generated password?");
   var passLowercase = confirm("Add Lowercase characters to your Generated password?");
   var passNumerical = confirm("Add Numbers to your Generated password?");
   var passSpecialcharacters = confirm("Add Special characters to your Generated password?");
 
   // This will confirm and pass the chosen variables picked by the user to the passString
-  if (upper === true ||
-    lower === true ||
-    numb === true ||
-    special === true) {
+  if (passUppercase === true ||
+    passLowercase === true ||
+    passNumerical === true ||
+    passSpecialcharacters === true) {
 
     if (passUppercase === true) {
-      passtring += upper
+      passString += upper
     }
 
     if (passLowercase === true) {
-      passtring += lower
+      passString += lower
     }
 
     if (passNumerical === true) {
-      passtring += numb
+      passString += numb
     }
 
     if (passSpecialcharacters === true) {
-      passtring += special
+      passString += special
+    }
+
+    // This will make sure the user chooses at least one of the modifiers provided
+    if (passUppercase != true &&
+      passLowercase != true &&
+      passNumerical != true &&
+      passSpecialcharacters != true) {
+      alert("Please Select one of these modifiers.");
+      generatePassword()
     }
 
   }
-  
-  // This will make sure the user chooses at least one of the modifiers provided
-  if (passUppercase != true &&
-    passLowercase != true &&
-    passNumerical != true &&
-    passSpecialcharacters != true) {
-    alert("Please Select one of these modifiers.");
-    generatePassword()
-  }
-
-  if (passLength <= 7 || passLength >= 129) {
-    alert("please choose a size between 8-128.");
-    generatePassword()
-
-  } else if (passLength > 7 || passLength < 129) {
-    alert("Password Generating");
-  }
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
