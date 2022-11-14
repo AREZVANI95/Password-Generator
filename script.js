75a3b237fc9944a19be53ddba8ad56a7be53d7af
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-11-14 01:02:14 -0500

    General Update
    Changed the path of the CSS in the HTML.
    Added prompts for the user to check for password length and if they want any of the 4 special characters added to their password.

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ function generatePassword() {
   var numbChar = "1234567890";
 
   // Prompts user for the Length of the password
-  var length = window.prompt("Enter Password Length: 8 - 128.");
+  var passLength = window.prompt("Enter Password Length: 8 - 128.");
+
+  //Converting passLength to a numerical value.
+  passLength = Number(passLength);
+
+  // Prompts user if password is empty.
+  if (passLength === "" || passLength === null) {
+    alert(" Please provide a valid answer!")
+    return newGeneratedPassword
+  } // Prompts user when number is out of range provided.
+  else if (passLength < 8 || passLength > 128) {
+    alert('Enter a valid number between 8-128')
+  } else if (isNaN(passLength)) {
+    alert('Numbers Only');
+  } else {
+    var upperCaseConfirm = confirm('Confirm to add uppercase characters to password'); // Prompts user for uppercase confirmation.
+    var lowerCaseConfirm = confirm('Confirm to add lowercase characters to password'); // Prompts user for lowercase confirmation.
+    var specialCaseConfirm = confirm('Confirm to add special characters to password'); // Prompts user for special character confirmation.
+    var numbCaseConfirm = confirm('Confirm to add number characters to password'); // Prompts user for number confirmation.
+  }
 
   return newGeneratedPassword
 };
